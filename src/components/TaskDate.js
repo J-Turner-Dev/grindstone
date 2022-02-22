d206429940282ac0b0b247bb3a415f3f76582276
J-Turner-Dev <jturner.dev@gmail.com>
2022-02-22 13:58:11 -0600

    Added ProjectOverlay and TaskDate components

---
+++ src/components/TaskDate.js
@@ -0,0 +1,46 @@
+import moment from "moment";
+import { FaSpaceShuttle, FaSun, FaRegPaperPlane } from "react-icons/fa";
+
+export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
+  showTaskDate && (
+    <div className="task-date" data-testid="task-date-overlay">
+      <ul className="task-date__list">
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(moment().format("MM/DD/YYYY"));
+          }}
+          data-testid="task-date-today"
+        >
+          <span>
+            <FaSpaceShuttle />
+          </span>
+          <span>Today</span>
+        </li>
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(moment().add(1, "day").format("DD/MM/YYYY"));
+          }}
+          data-testid="task-date-tomorrow"
+        >
+          <span>
+            <FaSun />
+          </span>
+          <span>Tomorrow</span>
+        </li>
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(moment().add(7, "days").format("DD/MM/YYYY"));
+          }}
+          data-testid="task-date-next-week"
+        >
+          <span>
+            <FaRegPaperPlane />
+          </span>
+          <span>Next week</span>
+        </li>
+      </ul>
+    </div>
+  );
